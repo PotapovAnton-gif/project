1ee69236d4391177ce480e3c2340c6ce1c1c232c
PotapovAnton-gif <58414983+PotapovAnton-gif@users.noreply.github.com>
2019-12-16 11:38:03 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,37 +2,36 @@
 #include <GL/glut.h>
 #include <cmath>
 using namespace std;
-double ang = 0, PI = 1.0471975512, w = 5.0, t = 0, Vx = 2 , Vy = 3, g = 9.81, x = 0, y = 0, a = 2, m = 2, b = 100.0;
-double const T = 3.06122, dt = 0.00001;
+double ang = 0, PI = 1.0471975512, w = 5.0, t = 0, Vx = 2 , Vy = 3, x = 0, y = 0, a = 2, m = 2, b = 1.0;
+double const T = 3.06122, dt = 0.0001, g = 9.81;
 
-int n = 0; 
+
 
 void sphera(double x, double y) {
 
 	glBegin(GL_TRIANGLES);
-	if (n % 8 == 0) {
-		glColor3f(1.0, 0.50, 0.30);
+	int n = 0;
+	while (n != 6*8) {
+		if (n % 2 == 0) {
+			glColor3f(1.0, 0.50, 0.30);
+		}
+		else {
+			glColor3f(0.50, 1.0, 0.30);
+		} n++;
+		ang += PI / 8;
+		glVertex2f(x, y);
+		glVertex2f(x + 0.1 * cos(ang + w * t), y + 0.1 * sin(ang + w * t));
+		glVertex2f(x + 0.1 * cos(ang + PI/8 + w * t), y + 0.1 * sin(ang + PI/8 + w * t));
 	}
-	else {
-		glColor3f(0.50, 1.0, 0.30);
-	} n++;
-	glVertex2f(x, y);
-	glVertex2f(x + 0.1 * cos(ang + w * t),y + 0.1 * sin(ang + w * t));
-	glVertex2f(x + 0.1 * cos(ang + PI / 8 + w * t),y + 0.1 * sin(ang + PI / 8 + w * t));
+	
 
 	glEnd();
 	glFlush();
-	if (n % 12 != 0) {
-		ang += PI / 8;
-		if (y >= 0) {
-			glutPostRedisplay();
-		} else{
-			cout << w;
-		}
-	}
+	
 }
 
 void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
 	t += dt;
 	if (Vy > 0) {
 		Vy -= g * dt + (Vy * a) / m * dt;
@@ -46,10 +45,9 @@ void display() {
 	if (w > 0) {
 		w -= ((w * b) / m) * dt;
 	}
+	
 	x += Vx * dt;
 	y += Vy * dt;
-	glBegin(GL_POINTS);
-	glColor3f(0.0, 0.0, 0.0);
 	sphera(x, y);
 	glEnd();
 	glFlush();
@@ -64,7 +62,7 @@ int main(int argc, char** argv) {
 	glutInitWindowSize(1080, 720);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Experement");
-	glClearColor(0.0, 0.0,01.0, 0);
+	glClearColor(0.0, 0.0, 0.0, 0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glOrtho(0.0, 1.0, 0.0, 1.0, 0.0, 0.0);
